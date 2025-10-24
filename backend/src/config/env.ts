003074292132194169615738f54ac2ea503873b1
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-10-23 20:39:41 -0400

    Added Working Supabase Sign Up and Sign In Endpoints

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,10 +2,5 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const {
-  PORT,
-  NODE_ENV,
-  SUPABASE_URL,
-  DB_URI,
-  SUPABASE_SERVICE_ROLE_KEY,
-} = process.env;
+export const { PORT, NODE_ENV, SUPABASE_URL, DB_URI, SUPABASE_ANON_KEY } =
+  process.env;
