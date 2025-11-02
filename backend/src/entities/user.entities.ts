6556a0c045b569d017b19f274041d9a85e990d73
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-11-02 18:47:00 -0500

    Added Base Forget Password and Reset Password Endpoints

---
+++ backend/src/entities/user.entities.ts
@@ -0,0 +1,69 @@
+import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
+import {
+  ACCESS_SECRET,
+  ACCESS_EXPIRES_IN,
+  REFRESH_SECRET,
+  REFRESH_EXPIRES_IN,
+} from "../config/env";
+
+/*
+  A high level abstraction of a user that encapsulates its core business logic while remaining independent 
+  of lower-level components of the application such as the database.  
+*/
+class User {
+  id: string;
+  name: string;
+  email: string;
+  provider_id: string | null;
+  avatar_url: string | null;
+
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    providerId: string | null = null,
+    avatarUrl: string | null = null
+  ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.provider_id = providerId;
+    this.avatar_url = avatarUrl;
+  }
+
+  // Handles given user password hashing
+  static async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  }
+
+  // Compares the given user password with the hashed password
+  async comparePassword(
+    plainPassword: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return bcrypt.compare(plainPassword, hashedPassword);
+  }
+
+  // Provides a token so a user can be authenticated
+  generateAccessToken(): string {
+    if (!ACCESS_SECRET) {
+      throw new Error("ACCESS_SECRET is not defined in environment variables");
+    }
+    return jwt.sign({ userId: this.id }, ACCESS_SECRET, {
+      expiresIn: (ACCESS_EXPIRES_IN || "15m") as any,
+    });
+  }
+
+  generateRefreshToken(): string {
+    if (!REFRESH_SECRET) {
+      throw new Error("REFRESH_SECRET is not defined in environment variables");
+    }
+    return jwt.sign({ userId: this.id }, REFRESH_SECRET, {
+      expiresIn: (REFRESH_EXPIRES_IN || "7d") as any,
+    });
+  }
+}
+
+export default User;
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { handleSignIn, handleSignUp } from "../controllers/auth.controller";
+import { authenticateUser } from "../middlewares/auth.middleware";
+import {
+  forgotPassword,
+  resetPassword,
+  signIn,
+  signOut,
+  signUp,
+} from "../controllers/auth.controller";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", handleSignUp);
-authRouter.post("/sign-in", handleSignIn);
+authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-out", authenticateUser, signOut);
+authRouter.post("/forgot-password", forgotPassword);
+authRouter.post("/reset-password", resetPassword);
 
 export default authRouter;
