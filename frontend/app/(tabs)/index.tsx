4f08a37613ac15a1d87c8497988d570910c72773
Fabian Ruiz <chapotincin@outlook.com>
2025-11-02 20:37:35 -0500

    Added routing to login and register from home page and globals

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from "expo-router";
+import { navigate } from "expo-router/build/global-state/routing";
+import { Text, View, Button } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Welcome to MyFi!</Text>
+      
+      <Button
+        onPress={() => navigate("/register")}
+        title="Create account"
+        color="#345995"
+      />
+
+      <Link href="/login">Already a member? Log in</Link>
+    </View>
+  );
+}
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)/index"
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="(tabs)/register"
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="(tabs)/login"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
+  )
 }
