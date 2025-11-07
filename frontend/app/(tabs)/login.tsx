84276c6a6976715c94a0fa0a36cf74dd24469d98
Fabian Ruiz <chapotincin@outlook.com>
2025-11-06 23:13:47 -0500

    Started login and register

--- frontend/app/(tabs)/login.tsx
+++ frontend/app/(tabs)/login.tsx
@@ -1,10 +1,15 @@
 import { Text, View } from "react-native";
 import React from 'react'
+import { Link } from "expo-router";
 
 const login = () => {
     return (
         <View className="flex-1 justify-center items-center">
-            <Text>Log In</Text>
+            <Text className="font-bold">Welcome Back!</Text>
+            <Text className="">Email Address</Text>
+            <Text className="">Password</Text>
+
+            <Link href="/register">Don't have an account? Sign Up</Link>
         </View>
     )
 }
--- frontend/app/(tabs)/register.tsx
+++ frontend/app/(tabs)/register.tsx
@@ -1,10 +1,17 @@
 import { Text, View } from "react-native";
 import React from 'react'
+import { Link } from "expo-router";
 
 const register = () => {
     return (
         <View className="flex-1 justify-center items-center">
-            <Text>Register</Text>
+            <Text className="font-bold">Get Started</Text>
+            <Text className="">First Name</Text>
+            <Text className="">Last Name</Text>
+            <Text className="">Email Address</Text>
+            <Text className="">Password</Text>
+
+            <Link href="/login">Already have an account? Log in</Link>
         </View>
     )
 }
