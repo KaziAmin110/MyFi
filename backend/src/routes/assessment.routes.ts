b7e5c2121411bf647f2dcdddcad2388a3a7d59cd
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-11-20 15:26:13 -0500

    Added Router Definition for onboarding intialization endpoint

--- backend/src/routes/assessment.routes.ts
+++ backend/src/routes/assessment.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { authenticateUser } from "../middlewares/auth.middleware";
+import { initializeOnboardingAssessment } from "../controllers/assessment.controller";
 
 const assessmentRouter = Router();
 
-assessmentRouter.post("/onboarding/initialize", authenticateUser);
+assessmentRouter.post(
+  "/onboarding/initialize",
+  authenticateUser,
+  initializeOnboardingAssessment
+);
 
 export default assessmentRouter;
